bf32438173cf4050a7ea15af314bfa59342a4083
paulesta55 <paul.estano@ensea.fr>
2019-09-29 10:59:51 +0200

    add submodules init

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/TestIJ
 unzip TrainIJCNN2013.zip -d .
 unzip TestIJCNN2013.zip -d .
 python ./traffic-detection.py --names_path=data/obj.names  --desc_path=TrainIJCNN2013/ReadMe.txt --in_img_path=TrainIJCNN2013 --out_img_path=data/obj --label_path=TrainIJCNN2013/gt.txt --list_file_path=data/train.txt
+git submodule init
+git submodule update
