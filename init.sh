7d14e1672d938c5da07d2c377d284a8b0528eea1
paulesta55 <paul.estano@ensea.fr>
2019-09-29 10:54:33 +0200

    add init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Initializing the project"
+wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/TrainIJCNN2013.zip
+wget https://sid.erda.dk/public/archives/ff17dc924eba88d5d01a807357d6614c/TestIJCNN2013.zip
+unzip TrainIJCNN2013.zip -d .
+unzip TestIJCNN2013.zip -d .
+python ./traffic-detection.py --names_path=data/obj.names  --desc_path=TrainIJCNN2013/ReadMe.txt --in_img_path=TrainIJCNN2013 --out_img_path=data/obj --label_path=TrainIJCNN2013/gt.txt --list_file_path=data/train.txt
