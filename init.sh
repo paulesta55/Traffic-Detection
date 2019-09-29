7aa0966560be0fb21230ed672604c9654a01819b
paulesta55 <paul.estano@ensea.fr>
2019-09-29 11:53:20 +0200

    add checkpoints to gitignore

--- init.sh
+++ init.sh
@@ -7,6 +7,9 @@ unzip TestIJCNN2013.zip -d .
 conda install -c conda-forge tqdm
 git submodule init
 git submodule update
+
+wget https://pjreddie.com/media/files/darknet53.conv.74 -P darknet/build/darknet/x64
+
 python ./traffic-detection.py --names_path=data/obj.names  --desc_path=TrainIJCNN2013/ReadMe.txt --in_img_path=TrainIJCNN2013 --out_img_path=data/obj --label_path=TrainIJCNN2013/gt.txt --list_file_path=data/train.txt
 
 mv TestIJCNN2013Download TestIJCNN2013
