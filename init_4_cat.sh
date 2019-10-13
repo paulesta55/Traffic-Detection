9d29ef40a88e8ba7e0d1535e453b5d04ed9c2a2d
paulesta55 <paul.estano@ensea.fr>
2019-10-13 16:59:46 +0200

    adapt for 4 categories

--- init_4_cat.sh
+++ init_4_cat.sh
@@ -6,4 +6,4 @@ conda install -c conda-forge tqdm
 git submodule init
 git submodule update
 wget https://pjreddie.com/media/files/darknet53.conv.74 -P darknet/build/darknet/x64
-python ./traffic-detection.py --names_path=data/obj.names  --desc_path=FullIJCNN2013/ReadMe.txt --in_img_path=FullIJCNN2013 --out_img_path=data/obj --label_path=FullIJCNN2013/gt.txt --list_file_dir=data --is_4_cat=False
+python ./traffic-detection.py --names_path=data/obj.names  --desc_path=FullIJCNN2013/ReadMe.txt --in_img_path=FullIJCNN2013 --out_img_path=data/obj --label_path=FullIJCNN2013/gt.txt --list_file_dir=data --is_4_cat=True
--- traffic-detection.py
+++ traffic-detection.py
@@ -12,10 +12,15 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 
 #python .\traffic-detection.py --names_path=data\obj.names  --desc_path=FullIJCNN2013\ReadMe.txt --in_img_path=FullIJCNN2013 --out_img_path=data\obj --label_path=FullIJCNN2013\gt.txt --list_file_dir=data
+pC = [0, 1, 2, 3, 4, 5, 7, 8, 9, 10, 15, 16]
+mC = [33, 34, 35, 36, 37, 38, 39, 40]
+dC = [11, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+
+ob_names="prohibitory\ndanger\nmandatory\nother"
 
 class DataLoader:
     
-    def __init__(self,names_path,desc_path,label_path,in_img_path,out_img_path,verbose,list_file_dir):
+    def __init__(self,names_path,desc_path,label_path,in_img_path,out_img_path,verbose,list_file_dir,is_4_cat):
         self.verbose = verbose
         self.names_path = names_path
         self.desc_path = desc_path
@@ -23,9 +28,13 @@ class DataLoader:
         self.out_img_path = out_img_path
         self.label_path = label_path
         self.list_file_dir = list_file_dir
-        self.gtsdb = pd.read_csv(os.path.abspath(label_path), sep=";", header=None,names=
+        self.gtsdb = pd.read_csv(os.path.abspath(label_path), sep=";",names=
                                  ["img", "x1", "y1", "x2", "y2","id"])
+        self.is_4_cat = is_4_cat
+        print(self.gtsdb.dtypes)
         self.complete_dataset()
+        if(self.is_4_cat):
+            self.class2cat()
         print(self.gtsdb.head())
         
     def complete_dataset(self):
@@ -35,7 +44,7 @@ class DataLoader:
                 if(self.verbose):
                     print(str.format("adding {0} to dataset",name))
                 image = Image.open(file_name)
-                self.gtsdb.loc[len(self)] = [name,None,None,None,None,None]
+                self.gtsdb.loc[len(self)] = [name,-1,-1,-1,-1,-1]
             
             
         
@@ -71,6 +80,10 @@ class DataLoader:
             f.write(class_name+os.linesep)
             
     def dump_classes(self):
+        if(self.is_4_cat):
+            with open(os.path.abspath(self.names_path), 'w') as names_file:
+                names_file.write(ob_names)
+            return
         lines = []
         with open(os.path.abspath(self.desc_path),'r') as desc_file:
             lines = desc_file.readlines()
@@ -83,6 +96,24 @@ class DataLoader:
             for n in names:
                 names_file.write(self.format_class_name(n))
    
+    def class2cat(self):
+#        class2catDict = {}
+#        for c,ids in enumerate([pC,mC,dC]):
+#            class2catDict.update({idx:c for idx in ids})    
+#       print(class2catDict)
+#       self.gtsdb.id = self.gtsdb.apply(lambda x:class2catDict[x.id] if x.id is not np.nan else None,axis=1)   
+        for index, row in self.gtsdb.iterrows():
+            if(row.id in pC):
+                self.gtsdb.loc[index,'id']=0
+            elif(row.id in mC):
+                self.gtsdb.loc[index,'id']=1
+            elif(row.id in dC):
+                self.gtsdb.loc[index,'id']=2
+            elif(row.id is -1 ):
+                self.gtsdb.loc[index,'id']=-1
+            else:
+                self.gtsdb.loc[index,'id']=3
+        
     def convert_all(self):
         cache = []
         for im in tqdm(self):
@@ -136,9 +167,10 @@ class DataLoader:
             with open(os.path.join(self.out_img_path,os.path.splitext(img_name)[0]+'.txt'),'w') as label_file:
                 for index, row in self.gtsdb.loc[self.gtsdb['img'] == img_name].iterrows():
                     example = self[index]
-                    if(example['label'] != None):
+                    if(example['label'] not in ['-1',-1,None,np.nan]):
                         bb_coordinates = self.get_bb_coordinates(example)
                         if(self.verbose):
+                            print(example)
                             print(index)
                             print(bb_coordinates)
                         sep = ' '
@@ -148,9 +180,6 @@ class DataLoader:
  
     
                 
-
-
-
 
 
 def main():
@@ -165,15 +194,17 @@ def main():
     parser.add_argument('--out_img_path',  type=str,required=True,help='path of the output directory for images and label files')
     parser.add_argument('--verbose',  type=bool,required=False,help='path of the gtsdb gt.txt file',default=False)
     parser.add_argument('--list_file_dir',type=str,required=True,help='path of the output file containing the list of the examples')
+    parser.add_argument('--is_4_cat',type=bool,required=True,help='specify if you are detecting gtsdb classes or categories(prohibitory,danger,mandatory or other')
     args = parser.parse_args()
     dataLoader = DataLoader(verbose=args.verbose,names_path=os.path.abspath(args.names_path),
         desc_path=os.path.abspath(args.desc_path),in_img_path=os.path.abspath(args.in_img_path),
         out_img_path= os.path.abspath(args.out_img_path),label_path=os.path.abspath(args.label_path),
-        list_file_dir=os.path.abspath(args.list_file_dir))
+        list_file_dir=os.path.abspath(args.list_file_dir),is_4_cat=args.is_4_cat)
     dataLoader.convert_all()
     dataLoader.dump_all()
     dataLoader.dump_classes()
     dataLoader.dump_split()
+
 
 
 if __name__ == '__main__':
